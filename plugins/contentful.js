f560c9cf285d62dce57f025df2b397f6c67deef3
Jacob Arriola <jacob@zeek.com>
2020-03-12 20:15:45 -0700

    Remove nuxt

--- plugins/contentful.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const contentful = require('contentful')
-
-module.exports = contentful.createClient({
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
-})
