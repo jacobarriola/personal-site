c78a137edd810bfca946ec16934cdfd8c472be13
Jacob Arriola <jacob.arriola@gmail.com>
2020-03-22 15:28:48 -0700

    Add skip to link

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,16 @@
-import 'typeface-inter'
-import './src/css/style.css'
+/**
+ * Set focus on skip to content if navigating from a previous page. Allows for easier toggling if navigating around
+ */
+exports.onRouteUpdate = ({ prevLocation }) => {
+  if (prevLocation === null) {
+    return
+  }
+
+  const skipLink = document.querySelector('#reach-skip-nav')
+
+  if (!skipLink) {
+    return
+  }
+
+  skipLink.focus()
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,28 @@
+import 'typeface-inter'
+import '../css/style.css'
+
 import PropTypes from 'prop-types'
 import React from 'react'
+import { SkipNavLink, SkipNavContent } from '@reach/skip-nav'
+import '@reach/skip-nav/styles.css'
 
 import Header from './header'
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col font-inter min-h-screen text-gray-900">
+      <SkipNavLink />
       <Header />
-
+      <SkipNavContent />
       <main className="flex flex-col flex-1 md:justify-center max-w-3xl mx-auto px-4 py-8 md:p-8 w-full">
         {children}
       </main>
-
       <footer className="border-t">
         <nav
           aria-label="footer nav"
           className="flex justify-between max-w-3xl mx-auto p-4 md:p-8 text-sm"
         >
-          <p>Jacob Arriola</p>
+          <p>Jacob Arriola &copy; {new Date().getFullYear()}</p>
           <ul className="flex">
             <li className="mr-4">
               <a
