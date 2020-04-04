df2947c70afbbde8260c11a990ab223abec36d7d
Jacob Arriola <jacob.arriola@gmail.com>
2020-04-04 15:46:50 -0700

    💅 Formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     extend: {
@@ -7,6 +8,8 @@ module.exports = {
       },
     },
   },
+
   variants: {},
+
   plugins: [require('@tailwindcss/custom-forms')],
 }
