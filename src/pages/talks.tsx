3eb9abe145131706c638f47ef5d5fd5abca02ec2
Jacob Arriola <jacob.arriola@gmail.com>
2021-01-08 20:11:35 -0800

    Add talks route

---
+++ src/pages/talks.tsx
@@ -0,0 +1,84 @@
+import { PageProps } from 'gatsby'
+import React from 'react'
+
+// Internal Dependencies
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import StructuredData from '../components/structured-data'
+
+const Talks: React.FC<PageProps> = () => {
+  return (
+    <Layout>
+      <SEO title="🗣 Talks" />
+      <StructuredData pageType="webPage" />
+      <main className="post-content md:text-xl">
+        <h1 className="text-3xl md:text-5xl mb-10 md:mb-20">Talks</h1>
+        <p>A collection of talks and appearances over the years.</p>
+        <ul className="grid grid-cols-1 gap-10 list-none">
+          <li>
+            <div className="responsive-embed">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/2qYn3VPcTpE"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </li>
+          <li>
+            <div className="responsive-embed">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/88DmiCugJV0"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </li>
+          <li>
+            <div className="responsive-embed">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/afEKFwGUiyE?start=207"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </li>
+          <li>
+            <div className="responsive-embed">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/B8SgHqlmkC8"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </li>
+          <li>
+            <div className="responsive-embed">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/w27KJ-4zV2w"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </li>
+        </ul>
+      </main>
+    </Layout>
+  )
+}
+
+export default Talks
